e3de0c31b78d6b6593cef90f30a1df6dd93f406c
Jonas Danilov <kriakiku@icloud.com>
2021-11-21 20:08:49 +0300

    feat: metric

--- app.vue
+++ app.vue
@@ -14,29 +14,31 @@
 </template>
 
 <script setup>
-(function (m, e, t, r, i, k, a) {
-  m[i] =
-    m[i] ||
-    function () {
-      (m[i].a = m[i].a || []).push(arguments);
-    };
-  m[i].l = 1 * new Date();
-  (k = e.createElement(t)),
-    (a = e.getElementsByTagName(t)[0]),
-    (k.async = 1),
-    (k.src = r),
-    a.parentNode.insertBefore(k, a);
-})(
-  window,
-  document,
-  "script",
-  "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js",
-  "ym"
-);
-ym(86583955, "init", {
-  clickmap: true,
-  trackLinks: true,
-  accurateTrackBounce: true,
-  webvisor: true,
-});
+if (process.server) {
+  (function (m, e, t, r, i, k, a) {
+    m[i] =
+      m[i] ||
+      function () {
+        (m[i].a = m[i].a || []).push(arguments);
+      };
+    m[i].l = 1 * new Date();
+    (k = e.createElement(t)),
+      (a = e.getElementsByTagName(t)[0]),
+      (k.async = 1),
+      (k.src = r),
+      a.parentNode.insertBefore(k, a);
+  })(
+    window,
+    document,
+    "script",
+    "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js",
+    "ym"
+  );
+  ym(86583955, "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+  });
+}
 </script>
